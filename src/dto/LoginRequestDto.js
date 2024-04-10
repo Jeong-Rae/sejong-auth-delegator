0af3fb93161e00a3f0b5fa03a89d6a5c439d4265
JeongRae <kkwjdfo@gmail.com>
2024-04-11 03:26:38 +0900

    [Docs] dto 주석 추가

--- src/dto/LoginRequestDto.js
+++ src/dto/LoginRequestDto.js
@@ -1,3 +1,9 @@
+/**
+ * 로그인 요청시 사용되는 dto
+ * @class
+ * @param {string} userId - sju 통합 로그인 ID
+ * @param {string} password - sju 통합 로그인 Password
+ */
 class LoginRequestDto {
     constructor(userId, password) {
         this.userId = userId;
