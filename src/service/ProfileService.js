2119bc41dfbfa168eee6f7d5423e025380f2006e
JeongRae <kkwjdfo@gmail.com>
2024-04-11 03:02:29 +0900

    [Feat] 유저 정보 서비스 구현

---
+++ src/service/ProfileService.js
@@ -0,0 +1,42 @@
+import ProfileResponseDto from "../dto/ProfileResponseDto.js";
+import cheerio from "cheerio";
+
+class UserProfileService {
+    constructor() {
+        this.profileEndpoint =
+            "http://classic.sejong.ac.kr/userCertStatus.do?menuInfoId=MAIN_02_05";
+    }
+
+    async fetchUserProfile(jsessionId) {
+        try {
+            const profileResponse = await fetch(this.profileEndpoint, {
+                method: "GET",
+                headers: {
+                    Cookie: `JSESSIONID=${jsessionId}`,
+                },
+                redirect: "follow",
+            });
+
+            const profileHtml = await profileResponse.text();
+            return this.parseProfileFromHtml(profileHtml);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    parseProfileFromHtml(html) {
+        const $ = cheerio.load(html);
+
+        const profileData = $("div.contentWrap li dl dd")
+            .map((_, el) => $(el).text().trim())
+            .get();
+
+        const profileResponseDto = new ProfileResponseDto(...profileData);
+
+        console.log(profileResponseDto);
+
+        return profileResponseDto;
+    }
+}
+
+export default UserProfileService;
