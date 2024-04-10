a991ae35dd918382f11aaee7e32c44c4a884bb94
JeongRae <kkwjdfo@gmail.com>
2024-04-11 03:27:18 +0900

    [Feat] 기능 분할 사용자 정보, 세션, 인증여부

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import AuthController from "./controllers/AuthController.js";
 import AuthService from "./services/AuthService.js";
 import UserProfileService from "./services/UserProfileService.js";
+import LoginRequestDto from "./dto/LoginRequestDto.js";
 
 class SejongAuthDelegator {
     constructor() {
@@ -12,8 +13,24 @@ class SejongAuthDelegator {
         );
     }
 
-    async login(loginRequestDto) {
+    async getAuthenticatedJsessionId(loginRequestDto) {
+        await this.authController.authenticate(loginRequestDto);
+        return this.authController.authService.jsessionId;
+    }
+
+    async isAuthenticatedUser(loginRequestDto) {
+        const isAuthenticated = await this.authController.authenticate(
+            loginRequestDto
+        );
+        return isAuthenticated;
+    }
+
+    async getUserProfile(loginRequestDto) {
         return this.authController.login(loginRequestDto);
+    }
+
+    createLoginRequestDto(userId, password) {
+        return new LoginRequestDto(userId, password);
     }
 }
 
--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -4,12 +4,22 @@ class AuthController {
         this.userProfileService = userProfileService;
     }
 
-    async login(loginRequestDto) {
-        await this.authService.authenticate(loginRequestDto);
-        const response = await this.userProfileService.fetchUserProfile(
-            authService.jsessionId
+    async authenticate(loginRequestDto) {
+        const isAuthenticated = await this.authService.authenticate(
+            loginRequestDto
         );
-        return response;
+        return isAuthenticated;
+    }
+
+    async login(loginRequestDto) {
+        const isAuthenticated = await this.authenticate(loginRequestDto);
+        if (isAuthenticated) {
+            const response = await this.userProfileService.fetchUserProfile(
+                this.authService.jsessionId
+            );
+            return response;
+        }
+        return null;
     }
 }
 
